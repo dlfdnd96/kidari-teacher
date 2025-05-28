39640de44c98b223d147ccd9fbe3731d909a7067
iwryu <dlfdnd96@gmail.com>
2025-05-28 21:27:43 +0900

    feat(project): 초기 프로젝트 구축

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	bracketSpacing: true,
+	trailingComma: 'all',
+}
---
+++ src/components/icons/index.ts
@@ -0,0 +1,1 @@
+export { NaverCafeIcon } from '@/components/icons/NaverCafeIcon'
