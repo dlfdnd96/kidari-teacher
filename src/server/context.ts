9f3e502c0fe972ad453e8f20485793f448bd5122
iwryu <dlfdnd96@gmail.com>
2025-06-07 15:26:11 +0900

    fix(nextjs): 완전 app route 방식으로 마이그레이션

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,10 +1,29 @@
 import type { CreateNextContextOptions } from '@trpc/server/adapters/next'
 import { prisma } from '@/lib/prisma'
 
-export const createTRPCContext = (opts: CreateNextContextOptions) => {
-	return {
-		...opts,
-		prisma,
+interface CreateAppContextOptions {
+	req: Request
+}
+
+type CreateContextOptions = CreateNextContextOptions | CreateAppContextOptions
+
+function isPagesRouterContext(
+	opts: CreateContextOptions,
+): opts is CreateNextContextOptions {
+	return 'res' in opts && 'query' in (opts as any).req
+}
+
+export function createTRPCContext(opts: CreateContextOptions) {
+	if (isPagesRouterContext(opts)) {
+		return {
+			...opts,
+			prisma,
+		}
+	} else {
+		return {
+			req: opts.req,
+			prisma,
+		}
 	}
 }
 
