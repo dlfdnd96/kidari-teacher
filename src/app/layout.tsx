abd22d2f48f0e10182568b802c519314051ea0be
iwryu <dlfdnd96@gmail.com>
2025-06-07 20:20:35 +0900

    fix(refactor): 상대경로를 절대경로로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '@/app/globals.css'
 import React from 'react'
 import { Inter } from 'next/font/google'
 import ClientLayout from '@/components/ClientLayout'
